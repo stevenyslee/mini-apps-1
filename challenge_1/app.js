5d018c2f8605e66c5b04d58301c5fc1788a71334
Steven Lee <Steven@Blueberry.local>
2018-07-09 12:33:58 -0700

    create board and add event listeners

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var moveCounter = 0;
+
+	var makeMove = function(){
+		if(!this.innerHTML) {
+			if (moveCounter % 2 === 0) {
+				this.innerHTML = 'X';
+				moveCounter++;
+			} else {
+				this.innerHTML = 'O';
+				moveCounter++;
+			}
+		}
+
+	};
+
+	var checkHorizontal = function() {
+
+	};
+
+	var checkDiagonal = function() {
+
+	};
+
+	var checkVertical = function() {
+
+	};
 
 
+	// Add event listen to all squares
+	var squares = [].slice.call(document.getElementsByClassName("square"));
+	squares.forEach(function (element){
+	  element.addEventListener('click', makeMove);
+	});
+
+});
