74858b54309f2530f7c0c148104d0d35b67d78af
Steven Lee <Steven@Blueberry.local>
2018-07-10 14:08:51 -0700

    change JSON to csv

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,61 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const path = require('path');
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.listen(3000, () => console.log('Web server listening on localhost:3000'));
+
+app.use(function(req, res, next) {
+  console.log(req.method, req.path);
+  next();
+});
+
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.post('/', (req, res) => {
+  res.statusCode = 201;
+  res.setHeader('Content-Type', 'text/csv');
+  
+  let keys = Object.keys(req.body);
+  keys.splice(keys.indexOf('children'), 1);
+  let people = [];
+  people.push(keys);
+
+  var getNestedData = function(obj) {
+    let person = [];
+    for (let i = 0; i < keys.length; i++) {
+      person.push(obj[keys[i]]);
+    }
+    people.push(person);
+    if (obj.children) {
+      for (let i = 0; i < obj.children.length; i++) {
+        getNestedData(obj.children[i]);
+      } 
+    }
+  };
+
+  getNestedData(req.body);
+
+  var createCSV = function(arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+      str += arr[i].join(',') +'\n';
+    }
+    return str;
+  }
+
+  var csvString = createCSV(people);
+  
+  
+  res.send(csvString);
+
+});
