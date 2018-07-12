57f9f5c3d36c75053175ed845a7b2ab34fc853cf
Steven Lee <Steven@Blueberry.local>
2018-07-12 16:43:40 -0700

    alternate players when dropping pieces

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -5,15 +5,26 @@ import ReactDOM from "react-dom";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {isToggleOn: true};
-    // this.handleClick = this.handleClick.bind(this);
+    this.state = {
+    	isPlayerOne: true
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 	handleClick(e, data) {
-		// console.log('click');
 		var element = e.currentTarget.id;
-		var rows = $('#' + element + '.col').parent();
-		for (let i = 1; i <= rows.length; i++) {
-			console.log($('#row' + i).find('#' + element));
+		// var rows = $('#' + element + '.col').parent();
+		for (let i = 1; i <= 6; i++) {
+			if (!$('#row' + i).find('#' + element)[0].innerHTML) {
+				if(this.state.isPlayerOne) {
+					$('#row' + i).find('#' + element)[0].innerHTML = 'X';
+					this.setState({isPlayerOne: !this.state.isPlayerOne});
+					break;
+				} else {
+					$('#row' + i).find('#' + element)[0].innerHTML = 'O';
+					this.setState({isPlayerOne: !this.state.isPlayerOne});
+					break;
+				}
+			}
 		}
 	}
 
@@ -22,57 +33,57 @@ export default class App extends React.Component {
 				<div id='container'>
 					<div class='row' id='row1'>
 						<div class='col' id='col1' onClick={this.handleClick}></div>
-						<div class='col' id='col2'></div>
-						<div class='col' id='col3'></div>
-						<div class='col' id='col4'></div>
-						<div class='col' id='col5'></div>
-						<div class='col' id='col6'></div>
-						<div class='col' id='col7'></div>
+						<div class='col' id='col2' onClick={this.handleClick}></div>
+						<div class='col' id='col3' onClick={this.handleClick}></div>
+						<div class='col' id='col4' onClick={this.handleClick}></div>
+						<div class='col' id='col5' onClick={this.handleClick}></div>
+						<div class='col' id='col6' onClick={this.handleClick}></div>
+						<div class='col' id='col7' onClick={this.handleClick}></div>
 					</div>
 					<div class='row' id='row2'>
-						<div class='col' id='col1'></div>
-						<div class='col' id='col2'></div>
-						<div class='col' id='col3'></div>
-						<div class='col' id='col4'></div>
-						<div class='col' id='col5'></div>
-						<div class='col' id='col6'></div>
-						<div class='col' id='col7'></div>
+						<div class='col' id='col1' onClick={this.handleClick}></div>
+						<div class='col' id='col2' onClick={this.handleClick}></div>
+						<div class='col' id='col3' onClick={this.handleClick}></div>
+						<div class='col' id='col4' onClick={this.handleClick}></div>
+						<div class='col' id='col5' onClick={this.handleClick}></div>
+						<div class='col' id='col6' onClick={this.handleClick}></div>
+						<div class='col' id='col7' onClick={this.handleClick}></div>
 					</div>
 					<div class='row' id='row3'>
-						<div class='col' id='col1'></div>
-						<div class='col' id='col2'></div>
-						<div class='col' id='col3'></div>
-						<div class='col' id='col4'></div>
-						<div class='col' id='col5'></div>
-						<div class='col' id='col6'></div>
-						<div class='col' id='col7'></div>
+						<div class='col' id='col1' onClick={this.handleClick}></div>
+						<div class='col' id='col2' onClick={this.handleClick}></div>
+						<div class='col' id='col3' onClick={this.handleClick}></div>
+						<div class='col' id='col4' onClick={this.handleClick}></div>
+						<div class='col' id='col5' onClick={this.handleClick}></div>
+						<div class='col' id='col6' onClick={this.handleClick}></div>
+						<div class='col' id='col7' onClick={this.handleClick}></div>
 					</div>
 					<div class='row' id='row4'>
-						<div class='col' id='col1'></div>
-						<div class='col' id='col2'></div>
-						<div class='col' id='col3'></div>
-						<div class='col' id='col4'></div>
-						<div class='col' id='col5'></div>
-						<div class='col' id='col6'></div>
-						<div class='col' id='col7'></div>
+						<div class='col' id='col1' onClick={this.handleClick}></div>
+						<div class='col' id='col2' onClick={this.handleClick}></div>
+						<div class='col' id='col3' onClick={this.handleClick}></div>
+						<div class='col' id='col4' onClick={this.handleClick}></div>
+						<div class='col' id='col5' onClick={this.handleClick}></div>
+						<div class='col' id='col6' onClick={this.handleClick}></div>
+						<div class='col' id='col7' onClick={this.handleClick}></div>
 					</div>
 					<div class='row' id='row5'>
-						<div class='col' id='col1'></div>
-						<div class='col' id='col2'></div>
-						<div class='col' id='col3'></div>
-						<div class='col' id='col4'></div>
-						<div class='col' id='col5'></div>
-						<div class='col' id='col6'></div>
-						<div class='col' id='col7'></div>
+						<div class='col' id='col1' onClick={this.handleClick}></div>
+						<div class='col' id='col2' onClick={this.handleClick}></div>
+						<div class='col' id='col3' onClick={this.handleClick}></div>
+						<div class='col' id='col4' onClick={this.handleClick}></div>
+						<div class='col' id='col5' onClick={this.handleClick}></div>
+						<div class='col' id='col6' onClick={this.handleClick}></div>
+						<div class='col' id='col7' onClick={this.handleClick}></div>
 					</div>
 					<div class='row' id='row6'>
-						<div class='col' id='col1'></div>
-						<div class='col' id='col2'></div>
-						<div class='col' id='col3'></div>
-						<div class='col' id='col4'></div>
-						<div class='col' id='col5'></div>
-						<div class='col' id='col6'></div>
-						<div class='col' id='col7'></div>
+						<div class='col' id='col1' onClick={this.handleClick}></div>
+						<div class='col' id='col2' onClick={this.handleClick}></div>
+						<div class='col' id='col3' onClick={this.handleClick}></div>
+						<div class='col' id='col4' onClick={this.handleClick}></div>
+						<div class='col' id='col5' onClick={this.handleClick}></div>
+						<div class='col' id='col6' onClick={this.handleClick}></div>
+						<div class='col' id='col7' onClick={this.handleClick}></div>
 					</div>
 				</div>
 			);
