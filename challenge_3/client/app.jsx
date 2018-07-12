669af45cab266c7e69ca75b100b5f087db656c84
Steven Lee <Steven@Blueberry.local>
2018-07-11 17:32:00 -0700

    create three landing pages

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,11 +1,101 @@
-class App extends React.Component {
+class User extends React.Component {
   render() {
     return (
       <div>
-        
+        <form>
+          Name:<br></br>
+          <input type="text" name="name"></input>
+          <br></br>Email:<br></br>
+          <input type="text" name="email"></input>
+          <br></br>Password:<br></br>
+          <input type="text" name="password"></input>
+          <br></br>
+          <input type="submit" value="Next"></input>
+          <br></br>
+        </form>
       </div>
     );
   }
+}
+
+class Address extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          Address Line 1:<br></br>
+          <input type="text" name="line-1"></input>
+          <br></br>Address Line 2:<br></br>
+          <input type="text" name="line-2"></input>
+          <br></br>City:<br></br>
+          <input type="text" name="city"></input>
+          <br></br>State:<br></br>
+          <input type="text" name="state"></input>
+          <br></br>Zip Code:<br></br>
+          <input type="text" name="zip"></input>
+          <br></br>Phone Number:<br></br>
+          <input type="text" name="phone"></input>
+          <br></br>
+          <input type="submit" value="Next"></input>
+          <br></br>
+        </form>
+      </div>
+    );
+  }
+}
+
+class Payment extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          Credit Card Number:<br></br>
+          <input type="text" name="credit"></input>
+          <br></br>Expiry Date:<br></br>
+          <input type="text" name="exp"></input>
+          <br></br>CVV:<br></br>
+          <input type="text" name="cvv"></input>
+          <br></br>Billing Zip Code:<br></br>
+          <input type="text" name="billing-zip"></input>
+          <br></br>
+          <input type="submit" value="Submit"></input>
+          <br></br>
+        </form>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: 0
+    }
+  }
+
+  render() {
+    if (this.state.form === 0) {
+      return (
+        <div>
+          <User />
+        </div>
+      ); 
+    } else if (this.state.form === 1) {
+      return (
+        <div>
+          <Address />
+        </div>
+      ); 
+    } else if (this.state.form === 2) {
+      return (
+        <div>
+          <Payment />
+        </div>
+      ); 
+    }
+  }
+
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
